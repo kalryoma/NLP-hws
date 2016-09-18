980eef42fc7ec251f14d0e319b18a0780d02f24b
AprilWong <aprilwang.zju@gmail.com>
2016-09-17 22:43:37 -0700

    after sort

--- segmenter/answer/mybaseline.py
+++ segmenter/answer/mybaseline.py
@@ -1,4 +1,5 @@
 import sys, codecs, optparse, os
+import heapq, math, operator
 
 # optparser : parse command-line
 optparser = optparse.OptionParser()
@@ -7,7 +8,6 @@ optparser.add_option("-b", "--bigramcounts", dest='counts2w', default=os.path.jo
 optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('data', 'input'), help="input file to segment")
 (opts, _) = optparser.parse_args()
 
-# this is for baseline???
 # Pdist is a python dictionary extract from counts1w or counts2w
 class Pdist(dict):
     "A probability distribution estimated from counts in datafile."
@@ -39,17 +39,52 @@ class Pdist(dict):
         elif len(key) == 1: return self.missingfn(key, self.N)
         else: return None
 
+# define entry
+class Entry(tuple):
+    def __new__(self, word, startposition, logprobability, backpointer):
+        Entry.w = property(operator.itemgetter(1))
+        Entry.sp = property(operator.itemgetter(2))
+        Entry.lp = property(operator.itemgetter(0))
+        Entry.bp = property(operator.itemgetter(3))
+        return tuple.__new__(Entry, (logprobability, word, startposition, backpointer))
+
 # the default segmenter does not use any probabilities, but you could ...
 Pw  = Pdist(opts.counts1w)
 
-
+# start my own codes
+# create an empty heap first
+with open(opts.input) as f:
+    flag = True
+    for line in f:
+        # initialize the heap
+        h = []
+        utf8line = unicode(line.strip(), 'utf-8')
+        # for each word that matches input at position 0
+        for word,freq in Pw.iteritems():
+            if(utf8line.find(word) == 0):
+                entry = Entry(word, 0, math.log10(Pw[word]), None)
+                heapq.heappush(h, entry)
+        # iteratively fill in chart[i] for all i
+        chart = []
+        heapsort(h)
+        if(len(h) == 2 and flag):
+            while(len(h) != 0):
+                tmp = heapq.heappop(h)
+                print tmp.w
+                print tmp.lp
+            flag = False
+            print utf8line
+        # while(h.size != 0):
+        #    h = heapsort(h)
+        #    entry = heap          
+    
 
 old = sys.stdout
 sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
 # ignoring the dictionary provided in opts.counts
-with open(opts.input) as f:
-    for line in f:
-        utf8line = unicode(line.strip(), 'utf-8')
-        output = [i for i in utf8line]  # segmentation is one word per character in the input
-        print " ".join(output)
+# with open(opts.input) as f:
+#    for line in f:
+#        utf8line = unicode(line.strip(), 'utf-8')
+#        output = [i for i in utf8line]  # segmentation is one word per character in the input
+#         print " ".join(output)
 sys.stdout = old
